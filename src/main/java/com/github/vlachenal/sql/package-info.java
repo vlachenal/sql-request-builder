f22554421aaf2465383b472242575cbaf16e26b9
Vincent Lachenal <vincent.lachenal@gmail.com>
2018-09-18 12:58:21 +0200

    Add package-info.java for Javadoc

---
+++ src/main/java/com/github/vlachenal/sql/package-info.java
@@ -0,0 +1,27 @@
+/*
+  SQL request builder:  Dynamic SQL request builder for java
+  Copyright (C) 2012 Vincent Lachenal
+
+  This file is part of SQL request builder.
+
+  SQL request builder is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  SQL request builder is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with SQL request builder. If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * Domain class used to provide SQL utilities.
+ *
+ * @since 0.1
+ *
+ * @author Vincent Lachenal
+ */
+package com.github.vlachenal.sql;
